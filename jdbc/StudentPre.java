e1cee7905307778505b32226689d5bf26d7984b6
Mudassar Quraishi <mq.mudassar@gmail.com>
2026-04-10 10:26:35 +0530

    insert/update functionality in StudentPre

--- jdbc/StudentPre.java
+++ jdbc/StudentPre.java
@@ -10,14 +10,28 @@ public class StudentPre {
 
 	public static void main(String[] args) {
 		String jdbc_driver="com.mysql.cj.jdbc.Driver";
-		String host="jdbc:mysql://localhost:3306/studentdb2";
+		String host="jdbc:mysql://localhost:3307/student";
 		String username="root";
 		String password="";
 		String query="select * from student where course=?";
+        String insert = "insert into student (name, course, marks) values(?, ?, ?)";
+        String update = "update student set marks=? where course =?";
 		
 		try {
 			Class.forName(jdbc_driver);
 			Connection con =DriverManager.getConnection(host,username,password);
+            //insert query
+		    PreparedStatement inputPst=con.prepareStatement(insert);
+            inputPst.setString(1, "Mohan");
+            inputPst.setString(2, "Btech");
+            inputPst.setLong(3, 85);
+            inputPst.executeUpdate();
+            //Update query
+            PreparedStatement updatePst = con.prepareStatement(update);
+            updatePst.setLong(1, 95);
+            updatePst.setString(2, "MCA");
+            updatePst.executeUpdate();
+            //Data reterival
 		    PreparedStatement pst=con.prepareStatement(query);
 		    pst.setString(1, "MCA");
 		    ResultSet rs= pst.executeQuery();
